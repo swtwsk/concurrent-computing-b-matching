c48680ea3e18f9dd970c48979369d1c3cc42b8e9
Andrzej Swatowski <andrzejswatowski@outlook.com>
2017-12-17 17:39:49 +0100

    Change to map and create Verticle struct

--- adorate.cpp
+++ adorate.cpp
@@ -5,22 +5,46 @@
 #include <fstream>
 #include <vector>
 #include <queue>
+#include <map>
 #include <limits>
-
-#define MAXN 1000000 // tu jest jazda, bo 10^9 to juz za duza tablica
 
 #define debug std::cout
 
 struct Edge {
-    unsigned int v; // Verticle the edge is pointing to
-    unsigned int weight; // Weight of the Edge
+    unsigned int v; // verticle the edge is pointing to
+    unsigned int weight; // weight of the Edge
 
     explicit Edge(unsigned int v, unsigned int weight) : v(v), weight(weight) {};
 };
+bool operator<(const Edge& a, const Edge& b) { // operator for MIN priority_queue
+    if (a.weight == b.weight) {
+        return a.v > b.v;
+    }
+    return a.weight > b.weight;
+}
+std::map<unsigned int, std::vector<Edge>> graph;
 
-std::vector<Edge> V[MAXN];
+struct Verticle {
+    unsigned int id;
+    unsigned int max_weight;
+    unsigned int bvalue;
+    std::priority_queue<Edge> S;
 
-unsigned int n = 0; // for debug purposes
+    explicit Verticle(unsigned int id, unsigned int b_val) : id(id), bvalue(b_val) {
+        max_weight = 0;
+        for (auto it = graph[id].begin(); it != graph[id].end(); ++it) {
+            max_weight = (*it).weight > max_weight ? (*it).weight : max_weight;
+        }
+    }
+
+    bool hasLast() {
+        return S.size() == bvalue;
+    }
+};
+bool operator<(const Verticle& a, const Verticle& b) {
+    return a.max_weight < b.max_weight;
+}
+std::priority_queue<Verticle> Q;
 
 void parseFile(std::string &input_filename) {
     std::ifstream file;
@@ -39,11 +63,8 @@ void parseFile(std::string &input_filename) {
                 file >> id_fvert >> id_svert >> weight;
                 file.ignore();
 
-                V[id_fvert].push_back(Edge(id_svert, weight));
-                V[id_svert].push_back(Edge(id_fvert, weight));
-
-                unsigned int max_id = id_fvert >= id_svert ? id_fvert : id_svert;
-                n = n < max_id ? max_id : n;
+                graph[id_fvert].push_back(Edge(id_svert, weight));
+                graph[id_svert].push_back(Edge(id_fvert, weight));
             }
             else {
                 file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -54,29 +75,39 @@ void parseFile(std::string &input_filename) {
     }
 }
 
+void prepareQueue(int b_method) {
+    for (auto m_it = graph.begin(); m_it != graph.end(); ++m_it) {
+        Q.push(Verticle((*m_it).first, bvalue(b_method, (*m_it).first)));
+    }
+}
+
+void sequentialAlgorithm(int b_method) {
+    prepareQueue(b_method);
+
+    while (!Q.empty()) {
+        auto vert = Q.top();
+        Q.pop();
+
+        debug << vert.id << " " << vert.bvalue << std::endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 4) {
         std::cerr << "usage: " << argv[0] << " thread-count inputfile b-limit" << std::endl;
         return 1;
     }
 
-    std::cout << "NO ELO" << std::endl;
-
     int thread_count = std::stoi(argv[1]);
     int b_limit = std::stoi(argv[3]);
     std::string input_filename{ argv[2] };
     parseFile(input_filename);
 
-    for (auto i = 0; i <= n; ++i) {
-        std::cout << i << ":\n";
-        for (auto it = V[i].begin(); it != V[i].end(); ++it) {
-            std::cout << " (" << (*it).v << ", " << (*it).weight << ")\n";
-        }
-    }
-
     for (int b_method = 0; b_method < b_limit + 1; b_method++) {
         // this is just to show the blimit with which the program is linked
         std::cerr << "bvalue node 44: " << bvalue(b_method, 44) << std::endl;
+
+        sequentialAlgorithm(b_method);
 
         // TODO: implement b-adorators here
 
--- blimit.cpp
+++ blimit.cpp
@@ -1,9 +1,20 @@
 #include "blimit.hpp"
 
-unsigned int bvalue(unsigned int method, unsigned long node_id) {
+/*unsigned int bvalue(unsigned int method, unsigned long node_id) {
     switch (method) {
     default: return node_id % 42;
     case 0: return 4;
     case 1: return 7;
     }        
+}*/
+
+unsigned int bvalue(unsigned int method, unsigned long node_id) {
+    switch (method) {
+    case 0: return 1;
+    default: switch (node_id) {
+        case 0: return 2;
+        case 1: return 2;
+        default: return 1;
+        }
+    }
 }
