d7c54a1ab42474fa84070cd2e213ef68e435f648
Andrzej Swatowski <andrzejswatowski@outlook.com>
2017-12-17 20:45:13 +0100

    Not working, preparing to fix

--- adorate.cpp
+++ adorate.cpp
@@ -11,10 +11,11 @@
 #define debug std::cout
 
 struct Edge {
-    unsigned int v; // verticle the edge is pointing to
+    int v; // verticle the edge is pointing to
     unsigned int weight; // weight of the Edge
+    bool used;
 
-    explicit Edge(unsigned int v, unsigned int weight) : v(v), weight(weight) {};
+    explicit Edge(int v, unsigned int weight) : v(v), weight(weight), used(false) {};
 };
 bool operator<(const Edge& a, const Edge& b) { // operator for MIN priority_queue
     if (a.weight == b.weight) {
@@ -22,17 +23,17 @@ bool operator<(const Edge& a, const Edge& b) { // operator for MIN priority_queu
     }
     return a.weight > b.weight;
 }
-std::map<unsigned int, std::vector<Edge>> graph;
+std::map<int, std::vector<Edge>> graph_edges;
 
 struct Verticle {
-    unsigned int id;
+    int id;
     unsigned int max_weight;
     unsigned int bvalue;
     std::priority_queue<Edge> S;
 
-    explicit Verticle(unsigned int id, unsigned int b_val) : id(id), bvalue(b_val) {
+    explicit Verticle(int id, unsigned int b_val) : id(id), bvalue(b_val) {
         max_weight = 0;
-        for (auto it = graph[id].begin(); it != graph[id].end(); ++it) {
+        for (auto it = graph_edges[id].begin(); it != graph_edges[id].end(); ++it) {
             max_weight = (*it).weight > max_weight ? (*it).weight : max_weight;
         }
     }
@@ -44,7 +45,8 @@ struct Verticle {
 bool operator<(const Verticle& a, const Verticle& b) {
     return a.max_weight < b.max_weight;
 }
-std::priority_queue<Verticle> Q;
+std::map<int, Verticle> graph_vert;
+std::priority_queue<Verticle *> Q;
 
 void parseFile(std::string &input_filename) {
     std::ifstream file;
@@ -56,15 +58,15 @@ void parseFile(std::string &input_filename) {
         while (file.get(c)) {
             if (c != '#') {
                 file.unget();
-                unsigned int id_fvert;
-                unsigned int id_svert;
+                int id_fvert;
+                int id_svert;
                 unsigned int weight;
 
                 file >> id_fvert >> id_svert >> weight;
                 file.ignore();
 
-                graph[id_fvert].push_back(Edge(id_svert, weight));
-                graph[id_svert].push_back(Edge(id_fvert, weight));
+                graph_edges[id_fvert].push_back(Edge(id_svert, weight));
+                graph_edges[id_svert].push_back(Edge(id_fvert, weight));
             }
             else {
                 file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -75,10 +77,28 @@ void parseFile(std::string &input_filename) {
     }
 }
 
-void prepareQueue(int b_method) {
-    for (auto m_it = graph.begin(); m_it != graph.end(); ++m_it) {
-        Q.push(Verticle((*m_it).first, bvalue(b_method, (*m_it).first)));
+inline void prepareQueue(int b_method) {
+    for (auto m_it = graph_edges.begin(); m_it != graph_edges.end(); ++m_it) {
+        auto id = (*m_it).first;
+        graph_vert.emplace(id, Verticle(id, bvalue(b_method, id)));
+        Q.push(&graph_vert.find(id)->second);
     }
+}
+
+std::vector<Edge>::iterator findMaxVerticle(int id) {
+    std::vector<Edge>::iterator eligible = graph_edges[id].begin(); // weight, v
+    bool found = false;
+
+    for (auto it = graph_edges[id].begin(); it != graph_edges[id].end(); ++it) {
+        if (!(*it).used) {
+            if ((*eligible).weight <= (*it).weight) {
+                eligible = it;
+                found = true;
+            }
+        }
+    }
+
+    return found ? eligible : graph_edges[id].end();
 }
 
 void sequentialAlgorithm(int b_method) {
@@ -88,7 +108,25 @@ void sequentialAlgorithm(int b_method) {
         auto vert = Q.top();
         Q.pop();
 
-        debug << vert.id << " " << vert.bvalue << std::endl;
+        //debug << vert.id << " " << vert.bvalue << std::endl;
+
+        while (vert->S.size() < vert->bvalue) {
+            auto x = findMaxVerticle(vert->id);
+            if (x == graph_edges[vert->id].end()) {
+                break;
+            }
+            else {
+                vert->S.push((*x));
+                (*x).used = true;
+                auto& to_propose = graph_vert[(*x).v];
+                auto& suitors = to_propose.S;
+                if (to_propose.hasLast()) {
+                    auto y = suitors.top();
+                    suitors.pop();
+                    suitors.push()
+                }
+            }
+        }
     }
 }
 
